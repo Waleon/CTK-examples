9fef24691699f498a66eac93c085175cafb3ce9c
wangliang <wangliang@paratera.com>
2018-05-09 12:34:17 +0800

    添加CTK示例代码

---
+++ HelloCTK/hello_activator.cpp
@@ -0,0 +1,12 @@
+﻿#include "hello_activator.h"
+#include "hello_impl.h"
+
+void HelloActivator::start(ctkPluginContext* context)
+{
+    s.reset(new HelloImpl(context));
+}
+
+void HelloActivator::stop(ctkPluginContext* context)
+{
+    Q_UNUSED(context)
+}
